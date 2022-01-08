6982c80ff8dd747ce57ae7975b5c23950d5d6ef1
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-01-08 06:08:05 -0500

    ...

--- dom-arrow.js
+++ dom-arrow.js
@@ -1,5 +1,26 @@
 import { CE } from 'trans-render/lib/CE.js';
 import { LeaderLine } from './leader-line.js';
+/**
+ * @tagName dom-arrow
+ * @element dom-arrow
+ * @prop {string} connect - The id of the element to connect from.
+ * @attr {string} connect - The id of the element to connect from.
+ * @prop {string} to - The id of the element to connect to.
+ * @attr {string} to - The id of the element to connect to.
+ * @prop {AreaAttachmentOptions} connectAreaAttachment - The area attachment options for the start of the connection.
+ * @attr {AreaAttachmentOptions} connect-area-attachment - The area attachment options for the start of the connection (json).
+ * @prop {AreaAttachmentOptions} toAreaAttachment - The area attachment options for the end of the  connection.
+ * @attr {AreaAttachmentOptions} to-area-attachment - The area attachment options for the end of the connection (json).
+ * @prop {PointAttachmentOptions} connectPointAttachment - The point attachment options for the start of the connection.
+ * @attr {PointAttachmentOptions} connect-point-attachment - The point attachment options for the start of the connection (json).
+ * @prop {PointAttachmentOptions} toPointAttachment - The point attachment options for the end of the connection.
+ * @attr {PointAttachmentOptions} to-point-attachment - The point attachment options for the end of the connection (json).
+ * @prop {number} loadDelay - The delay in milliseconds before the connection is drawn.
+ * @attr {number} load-delay - The delay in milliseconds before the connection is drawn.
+ * @prop {color} color - The color of the connection.
+ * @attr {color} color - The color of the connection.
+ *
+ */
 export class DOMArrowCore extends HTMLElement {
     doConnect(self) {
         const { connect, to, connectAreaAttachment, connectPointAttachment, toAreaAttachment, toPointAttachment, loadDelay } = self;
@@ -40,6 +61,9 @@ const ce = new CE();
 const configs = ['color', 'dash', 'dropShadow', 'endLabel', 'endPlug', 'endPlugColor', 'endPlugOutline', 'endPlugOutlineColor', 'endPlugOutlineSize', 'endPlugSize',
     'endSocket', 'endSocketGravity', 'gradient', 'hide', 'middleLabel', 'outline', 'outlineColor', 'outlineSize', 'path', 'show', 'size', 'startLabel',
     'startPlug', 'startPlugColor', 'startPlugOutlineSize', 'startPlugSize', 'startSocket', 'startPlugOutline', 'startPlugOutlineColor', 'startSocketGravity'];
+const stringProp = {
+    type: 'String'
+};
 ce.def({
     config: {
         tagName: 'dom-arrow',
@@ -52,14 +76,23 @@ ce.def({
             endPlugOutline: false,
             loadDelay: -1,
         },
+        propInfo: {
+            connect: stringProp,
+            to: stringProp,
+            color: stringProp,
+            startSocketGravity: stringProp,
+            endSocketGravity: stringProp,
+            startPlugColor: stringProp,
+            endPlugColor: stringProp,
+        },
         actions: {
             doConnect: {
                 ifAllOf: ['isC', 'connect', 'to'],
-                actIfKeyIn: ['connectAreaAttachment', 'connectPointAttachment', 'toAreaAttachment', 'toPointAttachment', 'loadDelay']
+                ifKeyIn: ['connectAreaAttachment', 'connectPointAttachment', 'toAreaAttachment', 'toPointAttachment', 'loadDelay']
             },
             doConfig: {
                 ifAllOf: ['line'],
-                actIfKeyIn: configs,
+                ifKeyIn: configs,
             }
         }
     },
--- dom-arrow.ts
+++ dom-arrow.ts
@@ -1,8 +1,29 @@
-import {CE} from 'trans-render/lib/CE.js';
+import {CE, PropInfo} from 'trans-render/lib/CE.js';
 import {DOMArrowProps, DOMArrowActions} from './types.js';
 import { LeaderLine} from './leader-line.js';
 import { Options, LeaderLine as LeaderLineType } from './leader-line-types.js';
 
+/**
+ * @tagName dom-arrow
+ * @element dom-arrow
+ * @prop {string} connect - The id of the element to connect from.
+ * @attr {string} connect - The id of the element to connect from.
+ * @prop {string} to - The id of the element to connect to.
+ * @attr {string} to - The id of the element to connect to.
+ * @prop {AreaAttachmentOptions} connectAreaAttachment - The area attachment options for the start of the connection.
+ * @attr {AreaAttachmentOptions} connect-area-attachment - The area attachment options for the start of the connection (json).
+ * @prop {AreaAttachmentOptions} toAreaAttachment - The area attachment options for the end of the  connection.
+ * @attr {AreaAttachmentOptions} to-area-attachment - The area attachment options for the end of the connection (json).
+ * @prop {PointAttachmentOptions} connectPointAttachment - The point attachment options for the start of the connection.
+ * @attr {PointAttachmentOptions} connect-point-attachment - The point attachment options for the start of the connection (json).
+ * @prop {PointAttachmentOptions} toPointAttachment - The point attachment options for the end of the connection.
+ * @attr {PointAttachmentOptions} to-point-attachment - The point attachment options for the end of the connection (json).
+ * @prop {number} loadDelay - The delay in milliseconds before the connection is drawn.
+ * @attr {number} load-delay - The delay in milliseconds before the connection is drawn.
+ * @prop {color} color - The color of the connection.
+ * @attr {color} color - The color of the connection.
+ * 
+ */
 export class DOMArrowCore extends HTMLElement implements DOMArrowActions{
     doConnect(self: this){
         const {
@@ -54,6 +75,9 @@ const configs = ['color', 'dash', 'dropShadow', 'endLabel','endPlug', 'endPlugCo
 'endSocket', 'endSocketGravity', 'gradient', 'hide', 'middleLabel', 'outline', 'outlineColor', 'outlineSize', 'path', 'show', 'size','startLabel',
 'startPlug','startPlugColor','startPlugOutlineSize','startPlugSize','startSocket','startPlugOutline','startPlugOutlineColor','startSocketGravity'] as (keyof DOMArrowProps & string)[];
 
+const stringProp: PropInfo = {
+    type: 'String'
+};
 ce.def({
     config:{
         tagName: 'dom-arrow',
@@ -66,14 +90,24 @@ ce.def({
             endPlugOutline: false,
             loadDelay: -1,
         },
+        propInfo:{
+            connect: stringProp,
+            to: stringProp,
+            color: stringProp,
+            startSocketGravity: stringProp,
+            endSocketGravity: stringProp,
+            startPlugColor: stringProp,
+            endPlugColor: stringProp,
+            
+        },
         actions:{
             doConnect:{
                 ifAllOf:['isC', 'connect', 'to'],
-                actIfKeyIn:['connectAreaAttachment', 'connectPointAttachment', 'toAreaAttachment', 'toPointAttachment', 'loadDelay']
+                ifKeyIn:['connectAreaAttachment', 'connectPointAttachment', 'toAreaAttachment', 'toPointAttachment', 'loadDelay']
             },
             doConfig:{
                 ifAllOf: ['line'],
-                actIfKeyIn: configs,
+                ifKeyIn: configs,
             }
         }
     },
